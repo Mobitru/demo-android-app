9f8fa4af9ce06e6e9494c124bea7a37fcb8f24df
dubovik-sergey <dubovik.siarhei@gmail.com>
2024-02-06 17:56:54 +0300

    add accessibility test automation based on Espresso Accessibility checks. also, fix several accessibility issues in style.xml

--- app/src/androidTest/java/com/epam/mobitru/AppBaseTest.java
+++ app/src/androidTest/java/com/epam/mobitru/AppBaseTest.java
@@ -14,7 +14,11 @@ import static androidx.test.espresso.matcher.ViewMatchers.supportsInputMethods;
 import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.google.android.apps.common.testing.accessibility.framework.AccessibilityCheckPreset.PRERELEASE;
+import static com.google.android.apps.common.testing.accessibility.framework.AccessibilityCheckResultUtils.matchesCheckNames;
+import static com.google.android.apps.common.testing.accessibility.framework.AccessibilityCheckResultUtils.matchesViews;
 import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -22,6 +26,7 @@ import android.util.Log;
 import android.view.View;
 
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.accessibility.AccessibilityChecks;
 import androidx.test.espresso.matcher.BoundedMatcher;
 
 import com.epam.mobitru.matchers.RViewMatcher;
@@ -29,6 +34,7 @@ import com.google.android.material.textfield.TextInputLayout;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
+import org.junit.BeforeClass;
 
 import java.time.Duration;
 import java.util.Optional;
@@ -44,6 +50,31 @@ public class AppBaseTest {
 
     protected static final String EMPTY = "";
 
+    @BeforeClass
+    public static void enableAccessibilityChecks() {
+        AccessibilityChecks.enable()
+                .setRunChecksFromRootView(true) // always check entire screen instead of specific view
+                .setCheckPreset(PRERELEASE) // use preset with maximum checks
+                .setSuppressingResultMatcher(anyOf(
+                        allOf(
+                                //example of suppressing the Speakable check for any Input
+                                matchesCheckNames(is("SpeakableTextPresentCheck")),
+                                matchesViews(supportsInputMethods())
+                        ), allOf(
+                                //example of suppressing the TargetSize check for specific elements
+                                matchesCheckNames(is("TouchTargetSizeCheck")),
+                                matchesViews(anyOf(withId(R.id.cart_title), withId(R.id.sortBy), withId(R.id.edit)))
+                        )
+                ));
+
+    }
+
+//UNCOMMENT below and comment previous one to perform all checks
+//    @BeforeClass
+//    public static void enableAccessibilityChecks() {
+//        AccessibilityChecks.enable()
+//                .setRunChecksFromRootView(true); // always check entire screen instead of specific view
+//    }
 
     protected void logout() {
         onView(allOf(withText("Account"), withEffectiveVisibility(VISIBLE))).perform(click());
